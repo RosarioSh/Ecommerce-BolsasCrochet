4d3984a2a499957736d015007690e988e95ed2f2
Rosario <rosario.sanchezhalumno.buap.mx>
2023-01-09 15:50:31 -0600

    Realizando ejemolos de variables stringm¿, number, boolean, indefinied, null, también una operación matemática con concatenación e interpolación y un ejemplo de comparación con if y else.

---
+++ JavaSc/nosotros.js
@@ -0,0 +1,35 @@
+//  OPERACION ARITMETICA
+// const edadUsuario1 = 20
+// const edadUsuario2 = 30
+
+// const resultado = edadUsuario1 + edadUsuario2;
+
+// alert ("edad de ambos usuarios" + resultado)
+
+
+//EJEMPLOS DE VARIABLES
+
+// STRING
+// let nombre = "Rosario";
+// console.log(nombre);
+
+// NUMBER
+// let edad = "30";
+// console.log(edad);
+
+// BOOLEAN
+// let cafe= "true";
+// console.log(cafe);
+// let cocacola= "false";
+// console.log(cocacola);
+
+
+//UNDEFINED 
+// let apellidoUsuario= "sin definir"
+// console.log(apellidoUsuario);
+
+
+//NULL
+let residencia = "null";
+console.log (residencia);
+
---
+++ JavaSc/principal.js
@@ -0,0 +1,29 @@
+// let nombreUsuario = prompt ("Hola ¿Cómo te llamas?");
+const edadUsuario = prompt ("¿Cuántos años tienes?");
+
+// console.log(nombreUsuario);
+
+// nombreUsuario = "Rosario"
+
+// console.log(edadUsuario);
+
+// edadUsuario = "30";
+
+// console.log(edadUsuario);
+
+
+//Concantenacion  
+
+// const saludo = alert("Hola" + nombreUsuario + ", tienes" + edadUsuario + "años de edad");
+
+//Interpolacion
+// const saludot = alert (`Hola ${nombreUsuario}, tienes ${edadUsuario} años de edad`);
+
+
+// Comparacion 
+if (edadUsuario > 20){
+    alert ("Puedes solicitar una promoción doble");
+} 
+else{
+    alert ("No puedes solicitar promociones,solo un descuento de 5%");
+}
