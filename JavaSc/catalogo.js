769e6c477cb75cd6a5e040bbf31ecba82b7cb8dd
Rosario <rosario.sanchezhalumno.buap.mx>
2023-02-14 18:31:39 -0600

    conociendo  eventos junto con forEach y listener

--- JavaSc/catalogo.js
+++ JavaSc/catalogo.js
@@ -1,19 +1,36 @@
-// let bolsas=[
-//     {id:1, bolsa:'Bolsa de Rafia',stock:3,precio:100},
-//     {id:2, bolsa:'Bolsa de Rafia',stock:4,precio:90},
-//     {id:3, bolsa:'Nylon',stock:5,precio:150},
-//     {id:4, bolsa:'Nylon',stock:5,precio:180},
-//     {id:5, bolsa:'Trapillo',stock:6,precio:200},
-//     {id:6, bolsa:'Trapillo',stock:5,precio:250}
-// ]
+const contenedorProductos = document.getElementById("contenedorProductos");
 
-// bolsas.forEach((producto) => {
-//     const div=document.createElement('div');
-//     div.classList.add('producto')
-//     div.innerHTML=`
-//     <h4>Marca: <p class="producto-agregado">${producto.bolsa}</p></h4>
-//     <h4>Stock: <p class="producto-agregado">${producto.stock}</p></h4>
-//     <h4>Precio: <p class="producto-agregado">${producto.precio}</p></h4>
-//     `
-//     contenedorProductos.appendChild(div)
-// })
+let bolsasT = [
+  { id: 1, bolsa: "Rafia", stock: 3, precio: 100 },
+  { id: 2, bolsa: "Rafia", stock: 4, precio: 90 },
+  { id: 3, bolsa: "Nylon", stock: 5, precio: 150 },
+  { id: 4, bolsa: "Nylon", stock: 5, precio: 180 },
+  { id: 5, bolsa: "Trapillo", stock: 6, precio: 200 },
+  { id: 6, bolsa: "Trapillo", stock: 5, precio: 250 },
+];
+
+bolsasT.forEach((item) => {
+  const div = document.createElement("div");
+  div.innerHTML = `
+    <h4> Bolsa: <p class="producto-agregado">${item.bolsa}</p></h4>
+    <h4> Stock: <p class="producto-agregado">${item.stock}</p></h4>
+    <h4> Precio: <p class="producto-agregado">${item.precio}</p></h4>
+    <div class ="contendedorProductos">
+    <button class="btn btnAgregarProducto" id="mostrarProductos${item.id}" Mostrar productos</button>
+  </div>
+  `;
+  contenedorProductos.appendChild(div);
+});
+
+  
+
+  const botonMostrarProductos=document.getElementById(`mostrarPrductos${item.id}`);
+  botonMostrarProductos.addEventListener("click", () => {
+    agregarProductos(item.id, botonMostrar);
+});
+
+  const botonOcultarProductos=document.getElementById(`ocultarPrductos${item.id}`);
+  botonOcultarProductos.addEventListener("click", () => {
+    agregarProductos(item.id, botonOcultar);
+});
+
